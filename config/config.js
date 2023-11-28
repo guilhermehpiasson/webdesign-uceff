38d535e9462796df79fce7a0181b396cd436e5af
Guilherme Henrique Piasson <guilhermehpiasson@gmail.com>
2023-11-28 15:41:57 -0300

    Ip change

--- config/config.js
+++ config/config.js
@@ -2,7 +2,7 @@ module.exports ={
     username: "vitor841_uceff_itp",
     password: "uceff_itp@23",
     database: "vitor841_uceff_itp",
-    host: "108.167.168.41",
+    host: "108.167.168.94",
     port:3306,
     dialect: "mysql",
     logging: true
